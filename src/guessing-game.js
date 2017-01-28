b0b4e2585042b3a402f2881988fb19ed8d5ea7a7
Anton Paskanny <anton.paskanny@gmail.com>
2017-01-28 19:11:20 +0300

    guessing-game/feature: impement guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this._arr = [];
+      this._min = null;
+      this._max = null;
+      this._guess = null;
+    }
 
     setRange(min, max) {
+      if (min < max ) {
 
+        while (min <= max) {
+          this._arr.push(min);
+          min++
+        }
+
+        this._min = 0;
+        this._max = this._arr.indexOf(max);
+      }
     }
 
     guess() {
-
+      this._guess = Math.round((this._min + this._max) / 2);
+      return this._guess;
     }
 
     lower() {
-
+      this._max = this._guess;
     }
 
     greater() {
-
+      this._min = this._guess;
     }
 }
 
